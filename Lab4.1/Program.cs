ce8b0c48ecf4276d9714b33f28cea7a756f730c7
Home <Home@DESKTOP-GOHQSKE>
2021-11-21 13:53:14 +0300

    Добавьте файлы проекта.

---
+++ Lab4.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0, e = 1;
+            Console.Write("Введите число N=");
+            int n = int.Parse(Console.ReadLine());
+            while (e <= n * 2)
+            {
+                sum = sum + e;
+                Console.WriteLine(sum);
+                e = e + 2;
+            }
+            Console.ReadKey();
+        }
+    }
+}
